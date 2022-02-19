b4efef0fb86a05ef9ef2687dfeb1badb85965f75
shun04101 <80952330+shun04101@users.noreply.github.com>
2022-02-19 08:14:51 +0000

    [Add]バリデーション/エラーメッセージ

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -11,9 +11,11 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     # current_userはdeviseのヘルパーメソッド
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
-
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   # 投稿一覧画面
